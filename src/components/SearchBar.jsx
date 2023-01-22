67eec06fc480b4a8ed3a23d8ab256aaa1914c41c
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-01-22 05:03:20 -0300

    SearchBar complete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useDebounce, useUpdateEffect } from 'usehooks-ts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import {getPokeName,searchPokemon} from '../redux/actions/index';
+import { setFiltersPokemon } from '../redux/actions/index';
 import '../assets/styles/components/SearchBar.scss';
 
 export default function SearchBar (){ 
     const dispatch = useDispatch()
     const search = useSelector((state) => state.search)
+    const [searchValue, setSearchValue] = useState(search)
+    const debouncedValue = useDebounce(searchValue, 300)
 
-    const handleSearch = (e) => {
-        dispatch(getPokeName(e))
-        dispatch(searchPokemon(e))
-    }
+    useUpdateEffect(() => {
+        dispatch(setFiltersPokemon(debouncedValue))
+    }, [debouncedValue])
+
+    useEffect(() => {
+        if (search === 'Remove filters') setSearchValue('')
+    }, [search])
 
     return(
-        <div className='searchBar'>
-            <input  type= 'text' value={search} placeholder="Search..." onChange={e => handleSearch(e.target.value)}/>
-            {search==="" && <FontAwesomeIcon icon={faMagnifyingGlass}/>}
+        <div className='search-bar'>
+            <input  type= 'text' value={searchValue} placeholder="Search..." onChange={e => setSearchValue(e.target.value)} maxLength={12}/>
+            {!searchValue && <FontAwesomeIcon icon={faMagnifyingGlass}/>}
         </div>
     )
 }
