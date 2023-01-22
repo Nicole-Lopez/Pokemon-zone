27d84ee81817d68e1c49677400566cb7d2577b9d
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-01-22 04:55:58 -0300

    type icon complete

--- src/components/TypeIcon.jsx
+++ src/components/TypeIcon.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import '../assets/styles/components/TypeIcon.scss'
+import '../assets/styles/components/TypeIcon.scss';
 
 export default function TypeIcon({icon, name}) {
 	return (
 		<div className='typeIcon'>
-			<img className='typeIcon__icon' src={icon} alt={name}/>
-			<span className='typeIcon__tooltip'>{name}</span>
+			<img src={icon} alt={name}/>
+			<span>{name}</span>
 		</div>
 	)
 }
