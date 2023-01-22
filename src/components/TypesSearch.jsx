6082fc76cdbbd4e522882d2a476a9d5ee321605a
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-01-22 04:58:33 -0300

    type search complete

--- src/components/TypesSearch.jsx
+++ src/components/TypesSearch.jsx
@@ -1,49 +1,51 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {getType} from "../redux/actions/index";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown,faChevronUp } from '@fortawesome/free-solid-svg-icons'
-import { useElementSize } from 'usehooks-ts'
-import '../assets/styles/components/Select.scss';
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getType } from "../redux/actions/index";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown,faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { useTrueElementSize } from 'use-true-element-size';
+import { useDebounce } from 'usehooks-ts';
 
-export default function TypesSearch({showType, handleSelectType, hidenANDshow, optionSelect, type, arrow, typeList}) {
+export default function TypesSearch({showType, handleSelectType, hidenANDshow, optionSelect, type, typeList}) {
  	const dispatch = useDispatch()
 	const types = useSelector((state) => state.typePokemon)
- 	const mobile = useSelector((state) => state.mobile);
+  	const divRef = useRef(null);
+  	const { elementHeight } = useTrueElementSize(divRef);
+	const [searchType, setSearchType] = useState('');   
+    const debouncedValue = useDebounce(searchType, 300)
 
-	const [searchType,setSearchType] = useState('');    
-  	const [squareRef, { height }] = useElementSize()
 
-  	useEffect(() => { 
-        dispatch(getType())
-  	},[dispatch])
-
-  	const handleClick = (e) => {
-  		// name:e.target.attributes.value.nodeValue
-  		// img:e.target.attributes[0].nodeValue
-  		handleSelectType(e,'type')
+  	const handleSelect = (e) => {
+  		// img:e.target.childNodes[1].src
+  		handleSelectType(e)
   		setSearchType('')
   	}
+  	const handleOpenSelect = (e) => {
+  		hidenANDshow(e)
+  		setSearchType('')  		
+  	}
+
+
+  	useEffect(() => { 
+  		if (!types.length) dispatch(getType())
+  	},[dispatch, types])
 
 	return (
 		<div className="select">              
-			<button className='select__option-main' onClick={(e)=>hidenANDshow(e)} value={type} type='button'>
+			<button className='select__option-main' type='button' onClick={handleOpenSelect} value={type}>
 				{optionSelect.icon && <img src={optionSelect.icon} alt={optionSelect.name}/>}	
-				{optionSelect.name}
-				{arrow && <FontAwesomeIcon className='arrow' icon={showType?faChevronUp:faChevronDown} />}
+				<span>{optionSelect.name}</span>
+				<FontAwesomeIcon icon={showType?faChevronUp:faChevronDown}/>
 			</button>
 
-			<div className={`select__options select__options--type ${showType && "open"}`} style={showType && mobile? {height: `${height + 50}px`}:null}>
-				<input type="text" placeholder='Search type...' onChange={e=>setSearchType(e.target.value)}/>
-				<div ref={squareRef}>
-					{types.filter(val=>{
-						if (searchType === '' || val.name.toLowerCase().includes(searchType.toLowerCase())) {
-							return val
-						} 
-					}).map(e=>{
+			<div className={`select__options select__options--type ${showType && "open"}`} style={showType? {height: `${elementHeight + 60}px`}:null}>
+				<input type="text" value={searchType} placeholder='Search type...' onChange={e=>setSearchType(e.target.value)}/>
+				<div ref={divRef}>
+					{types.filter(val => val.name.toLowerCase().includes(debouncedValue.toLowerCase()))
+					.map(e=>{
 						return(
-							<button image={e.icon} className={`${typeList.includes(e.name)?'disabled':null}`} type="button" key={e.id} onClick={(e)=>handleClick(e)} value={e.name}>
-								{e.name}<img src={e.icon} alt={e.name} value={e.name}/>
+							<button type="button" key={e.id} onClick={handleSelect} value={e.name} name='type' disabled={typeList.includes(e.name)}>
+								{e.name}<img src={e.icon} alt={e.name}/>
 							</button>
 						)
 					})}					
