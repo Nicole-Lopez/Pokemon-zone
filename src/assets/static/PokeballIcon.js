5e972c812aa57187278c6de2beabe8d472a0fb1f
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-01-22 03:44:58 -0300

    compressed assets

---
+++ src/assets/static/PokeballIcon.js
@@ -0,0 +1,10 @@
+export default function PokeballIcon() {
+	return (
+        <svg fill="none" height="30" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="30" xmlns="http://www.w3.org/2000/svg">
+        	<path d="M0 0h24v24H0z" fill="none" stroke="none"/>
+        	<circle cx="9" cy="9" r="9" transform="translate(3 3)"/>
+        	<circle cx="12" cy="12" r="3"/>
+        	<path d="M3 12h6m6 0h6"/>
+        </svg>
+	)
+}
