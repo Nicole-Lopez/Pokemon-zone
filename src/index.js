2dbeccdce63a0604ad06b1caa692165e697cf324
Nicole-Lopez <ananicolelopez96@gmail.com>
2023-01-23 19:21:44 -0300

    merge problem solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import './assets/reset.css';
 import './assets/globalStyles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
-import dotenv from 'dotenv';
-dotenv.config();
-
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
 	<React.StrictMode>
